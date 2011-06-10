b6d65615eadb05704dd5c789a6e356014052198f
akitty <patrickguan@gmail.com>
2011-06-10 16:28:21 -0700

    52 added

---
+++ p52.java
@@ -0,0 +1,15 @@
+class p52{
+	public static void main(String[]args){
+		int[][] dcount = new int[6][10];
+		
+		
+		for(int x = 1;;x++){
+			//digits of x
+			for(int i = 1;i<7;i++){
+				
+			
+			}
+		}
+		
+	}
+}
